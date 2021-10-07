05c23e28933b5230ae75f0c429f366a188ac64d5
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-10-07 18:19:28 +0300

    fixed script for creating .testcaferc.json

--- build.js
+++ build.js
@@ -1,4 +1,4 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.writeFileSync(path.resolve(__dirname, '.testcaferc.json'), '{}');
+fs.writeFileSync(path.resolve(__dirname.replace(/node_modules.*/, ''), '.testcaferc.json'), '{}');
